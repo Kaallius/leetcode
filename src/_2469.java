55e416652d1a2d61b1caab231d5967ad9b00fbc6
kaallius <enfor14677@naver.com>
2023-04-05 12:38:20 +0900

    math

---
+++ src/_2469.java
@@ -0,0 +1,11 @@
+public class _2469 {
+    public static class Solution {
+        public double[] convertTemperature(double celsius) {
+            double[] temps = new double[2];
+            temps[0] = celsius + 273.15;
+            temps[1] = celsius * 1.80 + 32.00;
+
+            return temps;
+        }
+    }
+}
